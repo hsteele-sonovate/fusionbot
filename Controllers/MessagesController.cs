2642dc912498351c54024df253ba8e7b15f15230
hsteele@sonovate.com <hsteele@sonovate.com>
2017-02-19 19:42:53 +0000

    First

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace Sonomate
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                // calculate something for us to return
+                int length = (activity.Text ?? string.Empty).Length;
+                
+                await Conversation.SendAsync(activity, () => new BasicLuisDialog());
+            }
+            else
+            {
+                await HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private async Task<Activity> HandleSystemMessage(Activity activity)
+        {
+            if (activity.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (activity.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+                var client = new ConnectorClient(new Uri(activity.ServiceUrl));
+                IConversationUpdateActivity update = activity;
+                if (update.MembersAdded.Any())
+                {
+                    var reply = activity.CreateReply();
+                    var newMembers = update.MembersAdded?.Where(t => t.Id != activity.Recipient.Id);
+                    foreach (var newMember in newMembers)
+                    {
+                        reply.Text = "Welcome";
+                        if (!string.IsNullOrEmpty(newMember.Name))
+                        {
+                            reply.Text += $" {newMember.Name}";
+                        }
+                        reply.Text += "!";
+                        await client.Conversations.ReplyToActivityAsync(reply);
+                    }
+                }
+            }
+            else if (activity.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (activity.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (activity.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Builder.FormFlow;
+using System.Linq;
+
+// For more information about this template visit http://aka.ms/azurebots-csharp-luis
+[Serializable]
+public class BasicLuisDialog : LuisDialog<object>
+{
+    public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute("0fe67cd2-68de-46f6-bc8b-a4867e97bdcd", "d4c81f2fc107452da3b7f979c22b101b")))
+    {
+    }
+
+    [LuisIntent("None")]
+    public async Task NoneIntent(IDialogContext context, LuisResult result)
+    {
+        await context.PostAsync($"You have reached the none intent. You said: {result.Query}"); //
+        context.Wait(MessageReceived);
+    }
+
+    [LuisIntent("Greet")]
+    public async Task GreetIntent(IDialogContext context, LuisResult result)
+    {
+        await context.PostAsync($"How can I help?"); //
+        context.Wait(MessageReceived);
+    }
+
+
+    [LuisIntent("GetPlacementHelp")]
+    public async Task MyIntent(IDialogContext context, LuisResult result)
+    {
+        await context.PostAsync($"Alright, I'll try and help.");
+        var frm = new PlacementForm();
+
+        var val = result.TopScoringIntent.Actions.FirstOrDefault()?.Parameters.FirstOrDefault()?.Value.FirstOrDefault().Resolution.FirstOrDefault().Value;
+        if (val != null)
+        {
+            frm.PlacementId = val;
+        }
+
+        var placementForm = new FormDialog<PlacementForm>(frm, PlacementForm.BuildForm, FormOptions.PromptInStart);
+        context.Call(placementForm, PlacementFormComplete);
+    }
+
+    public async Task PlacementFormComplete(IDialogContext ctx, IAwaitable<PlacementForm> result)
+    {
+        var placement = await result;
+        await ctx.PostAsync($"Let me have a look for placement {placement.PlacementId}");
+    }
+
+    [Serializable]
+    public class PlacementForm
+    {
+        [Prompt("What's the placement's id?")]
+        public string PlacementId { get; set; }
+
+        public static IForm<PlacementForm> BuildForm()
+        {
+            return new FormBuilder<PlacementForm>()
+                .Field(nameof(PlacementId))
+                .AddRemainingFields()
+                .Build();
+        }
+    }
+}
